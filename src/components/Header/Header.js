c0dbfa69625d648a7daade6d8e8d53e99ad609d6
AravindCT <aravind366@gmail.com>
2025-02-26 09:34:31 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,17 @@ import { Navbar, Nav } from 'react-bootstrap'
 export default function Header({ menuItems }) {
   return (
     <Navbar bg="white" expand="lg" sticky="top" className={styles.navbar}>
-      <Navbar.Brand href="/"><b>RAW</b><span className="text-primary">Graphs</span> 2.0</Navbar.Brand>
+    
+    <Navbar.Brand href="/" className="d-flex align-items-center">
+    <img 
+      src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Untitled%20design-qabRFqgDTHcpUQ8Clmq15CZU9F98Qw.png"
+      alt="Logo"
+      height="20"
+      width="20"
+      className="me-2 d-inline-block align-top"
+    />
+    
+      <b>Bharat</b><span className="text-primary">Stats </span> Viz</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
@@ -18,22 +28,14 @@ export default function Header({ menuItems }) {
           })}
           <a
             role="button"
-            href="https://github.com/rawgraphs/rawgraphs-app/issues"
+           href="https://www.bharatstats.com"
             target="_blank"
             rel="noopener noreferrer"
             className="btn btn-sm btn-primary ml-2 d-flex flex-column align-items-center justify-content-center"
           >
-            Report issue
+            Ask AI
           </a>
-          <a
-            role="button"
-            href="https://github.com/sponsors/rawgraphs"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-sm btn-primary ml-2 d-flex flex-column align-items-center justify-content-center"
-          >
-            Support us
-          </a>
+         
         </Nav>
       </Navbar.Collapse>
     </Navbar>
